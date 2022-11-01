16aa24d040b3e9d59f9a1a576dcb188eb95eead6
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2022-11-01 21:46:43 +0700

    update: enable zsh again for mac

--- systems/nix-darwin/maul/default.nix
+++ systems/nix-darwin/maul/default.nix
@@ -34,7 +34,7 @@
   # nix.package = pkgs.nix;
 
   # Create /etc/zshrc that loads the nix-darwin environment.
-  # programs.zsh.enable = true; # default shell on catalina
+  programs.zsh.enable = true; # default shell on catalina
   # programs.fish.enable = true;
 
   users.users.maul = {
